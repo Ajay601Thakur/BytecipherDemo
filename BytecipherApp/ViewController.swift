69d0e44895ddf233475ece5a24e1baa3b7421cda
ajay <ajay01493@gmail.com>
2022-07-15 00:23:11 +0530

    fixed Some Data

---
+++ BytecipherApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BytecipherApp
+//
+//  Created by Apple on 14/07/22.
+//
+
+import UIKit
+import BytecipherDemo
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor  = SwiftManager().getSomecolor()
+    }
+
+
+}
+
